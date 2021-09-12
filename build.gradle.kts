96fb4addefd4b1014279ee365ab07008024f29f1
John-DND <dnd.initiative.tracker@gmail.com>
2021-09-12 12:26:41 -0700

    now using shadow-mc-plugin 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import io.github.zap.build.gradle.convention.*
 
 // Uncomment to use local maven version - help local testing faster
 plugins {
-    id("io.github.zap.build.gradle.convention.shadow-mc-plugin") version "1.0.0-SNAPSHOT-1631472901"
+    id("io.github.zap.build.gradle.convention.shadow-mc-plugin") version "1.1.0"
 }
 
 repositories {
