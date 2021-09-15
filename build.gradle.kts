3086307b24050883a40a3a012de37e8913ee2c93
John-DND <dnd.initiative.tracker@gmail.com>
2021-09-15 00:22:57 -0700

    using latest arena-api

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
         exclude("net.kyori", "adventure-api")
     }
 
-    bukkitPlugin("io.github.zap:arena-api:1.0.0-SNAPSHOT-1631416054", qs())
+    bukkitPlugin("io.github.zap:arena-api:1.0.0-SNAPSHOT-1631690368", qs())
     bukkitPlugin("io.github.zap:zap-party:1.0.0-SNAPSHOT-1630956414", qs())
     bukkitPlugin("io.lumine.xikage:MythicMobs:4.12.0")
     bukkitPlugin("com.grinderwolf:slimeworldmanager-plugin:2.6.2-SNAPSHOT")
