fd53f473613b7596384072b88a8ed21853c677c2
John-DND <dnd.initiative.tracker@gmail.com>
2021-09-26 03:40:52 -0700

    bump zap-commons version

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 
 dependencies {
     paperApi ("1.16.5-R0.1-SNAPSHOT")
-    compileOnlyApi("io.github.zap:zap-commons:1.0.0-SNAPSHOT-1632458331", qs())
+    compileOnlyApi("io.github.zap:zap-commons:1.1.0", qs())
 
     implementation("com.grinderwolf:slimeworldmanager-api:2.6.2-SNAPSHOT")
     shade(project(":nms:nms-common"))
